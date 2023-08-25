39565b69d86c2cb7d4f70e34948d6385f05de3d8
JivaJR <ajavierr@uninorte.edu.co>
2023-08-24 21:04:09 -0500

    Barra desplegable

--- servidor/index.js
+++ servidor/index.js
@@ -26,24 +26,24 @@ app.get('/',(req,res) =>{
     res.send('Inicio servidor')
 })
 
-app.get('/enviar',(req,res) => {
-    var today = new Date();
-    var fecha = today.toLocaleDateString('it-IT');
-    var fecha = fecha.split('/').reverse().join('-');
-    var time = today.toLocaleTimeString('it-IT');
-    let lat = ("11"+"."+"0"+"0"+(Math.floor(Math.random() * (200000))));
-    let lon = ('-70'+"."+(Math.floor(Math.random() * (400000))));
-    let tabledb = 'gpsposition';
-    var sqlpet=`INSERT INTO ${tabledb} (IdEnvio, Fecha, Longitud, Latitud, Hora) VALUES (NULL,STR_TO_DATE('${fecha}','%Y-%m-%d'),${lon},${lat},STR_TO_DATE('${time}','%H:%i:%s'));`;
-    conexion.query(sqlpet, (err) => {
-        if (!err) {
-        console.log('Base de datos modificada exitosamente')
-        } else {
-        console.log(err);
-        }
-    })
-    res.redirect('./')
-})
+// app.get('/enviar',(req,res) => {
+//     var today = new Date();
+//     var fecha = today.toLocaleDateString('it-IT');
+//     var fecha = fecha.split('/').reverse().join('-');
+//     var time = today.toLocaleTimeString('it-IT');
+//     let lat = ("11"+"."+"0"+"0"+(Math.floor(Math.random() * (200000))));
+//     let lon = ('-70'+"."+(Math.floor(Math.random() * (400000))));
+//     let tabledb = 'gpsposition';
+//     var sqlpet=`INSERT INTO ${tabledb} (IdEnvio, Fecha, Longitud, Latitud, Hora) VALUES (NULL,STR_TO_DATE('${fecha}','%Y-%m-%d'),${lon},${lat},STR_TO_DATE('${time}','%H:%i:%s'));`;
+//     conexion.query(sqlpet, (err) => {
+//         if (!err) {
+//         console.log('Base de datos modificada exitosamente')
+//         } else {
+//         console.log(err);
+//         }
+//     })
+//     res.redirect('./')
+// })
 
 app.get('/recibir',(req,res) => {
     let tabledb = 'gpsposition';
@@ -53,6 +53,31 @@ app.get('/recibir',(req,res) => {
             data: mess,
         });
     });
+})
+
+app.get('/consultas',(req,res) => {
+    var {inicial,final} = req.query;
+    console.log(inicial,final)
+        let tabledb = 'gpsposition';
+
+        conexion.query(`SELECT * FROM ${tabledb} WHERE (Fecha BETWEEN '${inicial}' AND '${final}')`, (err, result) => {
+            if (!err) {
+                let info = result;
+                let latlon = Array(0);
+                let timeStamp = Array(0);
+                for (var i=0;i<info.length;i++){
+                    latlon[i] = [info[i]['Latitud'],info[i]['Longitud']];
+                    timeStamp[i] = [info[i]['Fecha'],info[i]['Hora']];
+                }
+                res.status(200).json({
+                    data: latlon,
+                    time:timeStamp
+                });
+            }else {
+            console.log(err);
+            }
+        })
+    // }
 })
 
 socket.on('message', (msg, rinfo) => {
