b7437891d7b7abf4e49a0804e7e5480b0e7830e3
maandrea-cm <maandrea@uninorte.edu.co>
2023-09-06 11:53:03 -0500

    nueva ec2

--- pagina-web/src/store/dates/thunks.js
+++ pagina-web/src/store/dates/thunks.js
@@ -5,7 +5,7 @@ import { searchingDates} from './datesSlice';
 export const searchDates = () => {
 
     return async(dispatch) => {
-        await fetch('http://localhost:8050/recibir')
+        await fetch('http://34.227.100.66:8050/recibir')
         .then(response => response.json())
         .then(data => {
             dispatch(searchingDates(data.data[0]));
